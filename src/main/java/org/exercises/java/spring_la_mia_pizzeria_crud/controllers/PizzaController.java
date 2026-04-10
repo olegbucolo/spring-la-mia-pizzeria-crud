d606b9368b0e8c619de208175eb385a4494702d6
olegbucolo <olegbucolo97@gmail.com>
2026-04-10 12:14:41 +0200

    END EX spring-la-mia-pizzeria-crud STEP 2 ADD VALIDATION

--- src/main/java/org/exercises/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/exercises/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -4,6 +4,7 @@ import org.exercises.java.spring_la_mia_pizzeria_crud.models.Pizza;
 import org.exercises.java.spring_la_mia_pizzeria_crud.repositories.PizzaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,45 +14,51 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/pizzas")
 public class PizzaController {
-    
+
     private final PizzaRepository pr;
 
-    public PizzaController(PizzaRepository pr){
+    public PizzaController(PizzaRepository pr) {
         this.pr = pr;
     }
 
     @GetMapping
-    public String index(Model model){
+    public String index(Model model) {
         model.addAttribute("pizzas", pr.findAll());
         return "pizzas";
     }
 
     @GetMapping("/{id}")
-    public String show(@PathVariable int id, Model model){
+    public String show(@PathVariable int id, Model model) {
         Pizza pizza = pr.findById(id)
-        .orElseThrow(() -> new RuntimeException("Pizza no found"));
+                .orElseThrow(() -> new RuntimeException("Pizza no found"));
         model.addAttribute("pizza", pizza);
         return "pizza-detail";
     }
 
     @GetMapping("/create")
-    public String create(Model model){
+    public String create(Model model) {
         model.addAttribute("pizza", new Pizza());
         return "pizza-create";
     }
 
     @PostMapping
-    public String store(@ModelAttribute Pizza pizza){
+    public String store(@Valid @ModelAttribute Pizza pizza,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.out.println("Errore nella creazione della pizza");
+            return "pizza-create";
+        }
         pr.save(pizza);
         return "redirect:/pizzas";
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable int id){
+    public String delete(@PathVariable int id) {
         pr.deleteById(id);
         return "redirect:/pizzas";
     }
@@ -64,10 +71,17 @@ public class PizzaController {
     }
 
     @PutMapping("/{id}")
-    public String editAtId(@PathVariable int id, @ModelAttribute Pizza pizza){
+    public String editAtId(
+            @PathVariable int id,
+            @Valid @ModelAttribute Pizza pizza,
+            BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            System.out.println("Consollogghi");
+            return "pizza-edit";
+        }
         pizza.setId(id);
         pr.save(pizza);
         return "redirect:/pizzas";
     }
-    
+
 }
--- src/main/java/org/exercises/java/spring_la_mia_pizzeria_crud/models/Pizza.java
+++ src/main/java/org/exercises/java/spring_la_mia_pizzeria_crud/models/Pizza.java
@@ -1,8 +1,8 @@
 package org.exercises.java.spring_la_mia_pizzeria_crud.models;
 
 import java.math.BigDecimal;
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 @Entity
 @Table(name = "pizzas")
@@ -10,47 +10,65 @@ public class Pizza {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
-    @Column(nullable = false)
+
+    @NotBlank
+    @Size(max = 100, message = "La lunghezza deve essere minore di 100 lettere")
+    @Column(nullable = false, length = 100)
     private String name;
 
-    @Column(length = 1000)
+    @NotBlank
+    @Size(max = 250, message = "La lunghezza deve essere minore di 250 lettere")
+    @Column(nullable = false, length = 250)
     private String description;
 
+    @Size(max = 500)
     private String imageUrl;
 
+    @NotNull
+    @DecimalMin(value = "0.01", inclusive = true, message = "il prezzo deve essere maggiore di 0")
     @Column(nullable = false, precision = 10, scale = 2)
+    @Digits(integer = 8, fraction = 2)
     private BigDecimal price;
 
-    public Pizza(){}
-    
+    public Pizza() {
+    }
+
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getImageUrl() {
         return imageUrl;
     }
+
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
+
     public BigDecimal getPrice() {
         return price;
     }
+
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
